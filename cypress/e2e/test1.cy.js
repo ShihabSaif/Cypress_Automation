79141628580bcc5d4c50ef78cbdea77337ab5676
A.B.M.Shihab Uddin <a.b.m.shihab57@gmail.com>
2023-10-23 21:15:48 +0600

    tallypay portal login automation added

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,4 +1,12 @@
-it('google test', function(){
-    cy.visit('https://google.com')
-    cy.get('.SDkEP').type("Bangladesh{enter}")
+it('TP Portal login', function(){
+    cy.visit('http://10.9.0.77:5333/#/login')
+
+    cy.get('.login-form > :nth-child(2) > :nth-child(1) > .form-control').type("shihab")
+    cy.get(':nth-child(2) > .form-control').type("Shihab12345!")
+
+    cy.get('.login-form > :nth-child(2) > .btn-container > .btn').click()
+
+    cy.get('.login-form > :nth-child(2) > :nth-child(2) > .input-group > .form-control').type("123456")
+
+    cy.get('.login-form > :nth-child(2) > .btn-container > .btn').click()
 })
