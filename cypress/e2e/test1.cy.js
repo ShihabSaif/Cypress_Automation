a038047dbae49aef47f095f8901a327519ecb9c6
A.B.M.Shihab Uddin <a.b.m.shihab57@gmail.com>
2023-10-23 23:45:12 +0600

    assetion added in tp portal login automation

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,12 +1,13 @@
-it('TP Portal login', function(){
+it('landing on tp portal', function(){
     cy.visit('http://10.9.0.77:5333/#/login')
+})
 
-    cy.get('.login-form > :nth-child(2) > :nth-child(1) > .form-control').type("shihab")
-    cy.get(':nth-child(2) > .form-control').type("Shihab12345!")
-
-    cy.get('.login-form > :nth-child(2) > .btn-container > .btn').click()
-
-    cy.get('.login-form > :nth-child(2) > :nth-child(2) > .input-group > .form-control').type("123456")
-
-    cy.get('.login-form > :nth-child(2) > .btn-container > .btn').click()
+it('TP Portal login', function(){
+    cy.visit('http://10.9.0.77:5333/#/login') //tp portal login page land
+    cy.get('.login-form > :nth-child(2) > :nth-child(1) > .form-control').type("shihab") //username
+    cy.get(':nth-child(2) > .form-control').type("Shihab12345!") //password
+    cy.get('.login-form > :nth-child(2) > .btn-container > .btn').click()  //submit button
+    cy.get('.login-form > :nth-child(2) > :nth-child(2) > .input-group > .form-control').type("123456") //otp
+    cy.get('.login-form > :nth-child(2) > .btn-container > .btn').click()  //confirm button
+    cy.get('h3').should('contain','Welcome to TallyPay Portal')
 })
