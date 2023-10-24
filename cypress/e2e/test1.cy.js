fde1b29cbabb5d2e4a0f3e70242fe3c07788406b
A.B.M.Shihab Uddin <a.b.m.shihab57@gmail.com>
2023-10-24 14:16:15 +0600

    implemented POM for tp portal login page automation

--- cypress/e2e/test1.cy.js
+++ cypress/e2e/test1.cy.js
@@ -1,13 +1,13 @@
-it('landing on tp portal', function(){
-    cy.visit('http://10.9.0.77:5333/#/login')
-})
+import { LoginPage } from "../page/LoginPage"
+
+const loginPage = new LoginPage()
 
 it('TP Portal login', function(){
-    cy.visit('http://10.9.0.77:5333/#/login') //tp portal login page land
-    cy.get('.login-form > :nth-child(2) > :nth-child(1) > .form-control').type("shihab") //username
-    cy.get(':nth-child(2) > .form-control').type("Shihab12345!") //password
-    cy.get('.login-form > :nth-child(2) > .btn-container > .btn').click()  //submit button
-    cy.get('.login-form > :nth-child(2) > :nth-child(2) > .input-group > .form-control').type("123456") //otp
-    cy.get('.login-form > :nth-child(2) > .btn-container > .btn').click()  //confirm button
-    cy.get('h3').should('contain','Welcome to TallyPay Portal')
+    loginPage.navigate()
+    loginPage.username()
+    loginPage.password()
+    loginPage.submitBtn()
+    loginPage.otp()
+    loginPage.confirmBtn()
+    loginPage.welcomeAfterLogin()
 })
---
+++ cypress/page/LoginPage.js
@@ -0,0 +1,30 @@
+export class LoginPage {
+    navigate()
+    {
+        cy.visit('http://10.9.0.77:5333/#/login') //tp portal login page land
+    }
+    username()
+    {
+        cy.get('.login-form > :nth-child(2) > :nth-child(1) > .form-control').type("shihab") //username
+    }
+    password()
+    {
+        cy.get(':nth-child(2) > .form-control').type("Shihab12345!") //password
+    }
+    submitBtn()
+    {
+        cy.get('.login-form > :nth-child(2) > .btn-container > .btn').click()  //submit button
+    }
+    otp()
+    {
+        cy.get('.login-form > :nth-child(2) > :nth-child(2) > .input-group > .form-control').type("123456") //otp
+    }
+    confirmBtn()
+    {
+        cy.get('.login-form > :nth-child(2) > .btn-container > .btn').click()  //confirm button
+    }
+    welcomeAfterLogin()
+    {
+        cy.get('h3').should('contain','Welcome to TallyPay Portal')
+    }
+}
