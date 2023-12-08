9a7588e527e9195533e80c26f6b537ec4da888ca
A.B.M.Shihab Uddin <a.b.m.shihab57@gmail.com>
2023-12-09 00:28:15 +0600

    read file using readFile method

--- cypress/e2e/fileRead.cy.js
+++ cypress/e2e/fileRead.cy.js
@@ -1,5 +1,13 @@
-it('read file', function(){
+it('read file using fixture', function(){
     cy.fixture('example.json').then((data) => {
+        cy.log(data.name)
+        cy.log(data.email)
+        cy.log(data.body)
+    } )
+})
+
+it('read file using readFile', function(){
+    cy.readFile('./cypress/fixtures/example.json').then((data) => {
         cy.log(data.name)
         cy.log(data.email)
         cy.log(data.body)
