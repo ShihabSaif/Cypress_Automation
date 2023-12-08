5f760288513f4778d43e2d716394f59f2d2b9220
A.B.M.Shihab Uddin <a.b.m.shihab57@gmail.com>
2023-12-09 00:39:15 +0600

    write file using writeFile method

--- cypress/e2e/fileRead.cy.js
+++ cypress/e2e/fileRead.cy.js
@@ -13,3 +13,9 @@ it('read file using readFile', function(){
         cy.log(data.body)
     } )
 })
+
+it('write file', function(){
+    cy.writeFile('text.txt', 'Hello World \n')
+
+    cy.writeFile('text.txt', 'I am Shihab', {flag: 'a+'})
+})
