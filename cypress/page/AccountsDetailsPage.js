e07ead66d8233129eeb7c94acdce85d9490e63c3
A.B.M.Shihab Uddin <a.b.m.shihab57@gmail.com>
2025-08-30 14:44:18 +0600

    cy wait removed

--- cypress/page/AccountsDetailsPage.js
+++ cypress/page/AccountsDetailsPage.js
@@ -1,38 +1,30 @@
 import { expect } from "chai"
 export class AccountsDetailsPage{
-    AccountsOption = '.is-expanded > .app-menu__item > .app-menu__label'
-    AccountsDetailsOption = '.is-expanded > .treeview-menu > :nth-child(1) > .treeview-item'
 
     clickAccountsOption()
     {
         cy.contains('span.app-menu__label', 'Accounts').click()
-        cy.wait(2000)
     }
 
     clickAccountsDetailsOption()
     {
         cy.contains('a.treeview-item', 'Accounts Detail').click()
-        cy.wait(2000)
     }
 
     walletInput()
     {
         cy.get('input[type="text"]').first().type('01621215877');
-        cy.wait(2000)
     }
     submitButton()
     {
         cy.contains('button', 'Search').click();
-        cy.wait(2000)
     }
     txnInfoDetailsButton()
     {
         cy.contains('a', 'Txn Info').click();
-        cy.wait(2000)
     }
     walletInformationClick()
     {
         cy.get('i.fas.fa-external-link-alt').parent().click()
-        cy.wait(2000)
     }
 }
--- cypress/page/LoginPage.js
+++ cypress/page/LoginPage.js
@@ -13,37 +13,26 @@ export class LoginPage {
     navigate()
     {
         cy.visit(this.tp_portal_link) //tp portal login page land
-        cy.wait(2000)
     }
     username(username)
     {
         cy.get(this.username_textbot).type(username) //username
-        cy.wait(2000)
     }
     password(password)
     {
         cy.get(this.password_textbox).type(password) //password
-        cy.wait(2000)
     }
     submitBtn()
     {
         cy.get(this.submit_button).click()  //submit button
-        cy.wait(5000)
     }
-    otp()
+    otp(otp_value)
     {
-        cy.get(this.otp_textbox).type("123456") //otp
-        cy.wait(2000)
+        cy.get(this.otp_textbox).type(otp_value) //otp
     }
     confirmBtn()
     {
         cy.get(this.confirm_button).click()  //confirm button
-        cy.wait(2000)
+        cy.wait(1000)
     }
-    welcomeAfterLogin()
-    {
-        cy.get('h3').should('contain','Welcome to TallyPay Portal')
-        cy.wait(2000)
-    }
-
 }
